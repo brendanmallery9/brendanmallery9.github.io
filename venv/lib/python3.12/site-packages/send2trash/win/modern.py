22d1c8255a18a6fb3c9b9918d3a392b4fcef93c2
brendanmallery9 <96560435+brendanmallery9@users.noreply.github.com>
2026-01-06 20:54:05 +0000

    Deploying to gh-pages from @ brendanmallery9/brendanmallery9.github.io@fa6ff552c1ce266b7601ec150f5bea6b8040c155 🚀

--- venv/lib/python3.12/site-packages/send2trash/win/modern.py
+++ venv/lib/python3.12/site-packages/send2trash/win/modern.py
@@ -6,12 +6,11 @@
 
 from __future__ import unicode_literals
 import os.path as op
-from send2trash.compat import text_type
-from send2trash.util import preprocess_paths
 from platform import version
 import pythoncom
 import pywintypes
 from win32com.shell import shell, shellcon
+from send2trash.util import preprocess_paths
 from send2trash.win.IFileOperationProgressSink import create_sink
 
 
@@ -20,7 +19,7 @@ def send2trash(paths):
     if not paths:
         return
     # convert data type
-    paths = [text_type(path, "mbcs") if not isinstance(path, text_type) else path for path in paths]
+    paths = [str(path, "mbcs") if not isinstance(path, str) else path for path in paths]
     # convert to full paths
     paths = [op.abspath(path) if not op.isabs(path) else path for path in paths]
     # remove the leading \\?\ if present
@@ -60,7 +59,7 @@ def send2trash(paths):
     except pywintypes.com_error as error:
         # convert to standard OS error, allows other code to get a
         # normal errno
-        raise OSError(None, error.strerror, path, error.hresult)
+        raise OSError(None, error.strerror, path, error.hresult) from error
     finally:
         # Need to make sure we call this once fore every init
         pythoncom.CoUninitialize()
--- venv/lib64/python3.12/site-packages/send2trash/win/legacy.py
+++ venv/lib64/python3.12/site-packages/send2trash/win/legacy.py
@@ -6,10 +6,6 @@
 
 from __future__ import unicode_literals
 import os.path as op
-
-from send2trash.compat import text_type
-from send2trash.util import preprocess_paths
-
 from ctypes import (
     windll,
     Structure,
@@ -21,6 +17,8 @@ from ctypes import (
     FormatError,
 )
 from ctypes.wintypes import HWND, UINT, LPCWSTR, BOOL
+
+from send2trash.util import preprocess_paths
 
 kernel32 = windll.kernel32
 GetShortPathNameW = kernel32.GetShortPathNameW
@@ -143,7 +141,7 @@ def send2trash(paths):
     if not paths:
         return
     # convert data type
-    paths = [text_type(path, "mbcs") if not isinstance(path, text_type) else path for path in paths]
+    paths = [str(path, "mbcs") if not isinstance(path, str) else path for path in paths]
     # convert to full paths
     paths = [op.abspath(path) if not op.isabs(path) else path for path in paths]
     # get short path to handle path length issues
